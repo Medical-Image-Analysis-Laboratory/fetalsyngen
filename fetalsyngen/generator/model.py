44d715bbc59217229ac3c253f36f471ce8374d23
Thomas Sanchez <sanchez.thomas056@gmail.com>
2025-04-25 09:30:43 +0200

    Restructure the generator to have a separate image generation (intensity + deformation) and an augmentation function. This is quite important for BrainID applications

--- fetalsyngen/generator/model.py
+++ fetalsyngen/generator/model.py
@@ -93,6 +93,147 @@ class FetalSynthGen:
             if value is not None
         }
 
+    def generate(
+        self,
+        image: torch.Tensor | None,
+        segmentation: torch.Tensor,
+        seeds: torch.Tensor | None,
+        genparams: dict = {},
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, dict]:
+        """
+        Generate a synthetic deformed image from the input data.
+        Supports both random generation and from a fixed genparams dictionary.
+
+        Args:
+            image: Image to use as intensity prior if required.
+            segmentation: Segmentation to use as spatial prior.
+            seeds: Seeds to use for intensity generation.
+            genparams: Dictionary with generation parameters.
+                Used for fixed generation.
+                Should follow the structure and be of the same type as
+                the returned generation parameters.
+
+        Returns:
+            The synthetic deformed image, the deformed segmentation, the original image, and the generation parameters.
+
+        """
+
+        # 1. Generate intensity output.
+        if seeds is not None:
+            seeds, selected_seeds = self.intensity_generator.load_seeds(
+                seeds=seeds, genparams=genparams.get("selected_seeds", {})
+            )
+            output, seed_intensities = (
+                self.intensity_generator.sample_intensities(
+                    seeds=seeds,
+                    device=self.device,
+                    genparams=genparams.get("seed_intensities", {}),
+                )
+            )
+        else:
+            if image is None:
+                raise ValueError(
+                    "If no seeds are passed, an image must be loaded to be used as intensity prior!"
+                )
+            # normalize the image from 0 to 255 to
+            # match the intensity generator
+            output = (image - image.min()) / (image.max() - image.min()) * 255
+            selected_seeds = {}
+            seed_intensities = {}
+
+        # ensure that tensors are on the same device
+        output = output.to(self.device)
+        segmentation = segmentation.to(self.device)
+        image = image.to(self.device) if image is not None else None
+
+        # 2. Spatially deform the data
+        image, segmentation, output, deform_params = (
+            self.spatial_deform.deform(
+                image=image,
+                segmentation=segmentation,
+                output=output,
+                genparams=genparams.get("deform_params", {}),
+            )
+        )
+        synth_params = {
+            "selected_seeds": selected_seeds,
+            "seed_intensities": seed_intensities,
+            "deform_params": deform_params,
+        }
+        return output, segmentation, image, synth_params
+
+    def augment(
+        self,
+        image: torch.Tensor | None,
+        segmentation: torch.Tensor,
+        seeds: torch.Tensor | None,
+        genparams: dict = {},
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, dict]:
+        """
+        Generate a randomized augmentations on the synthetic image from the input data.
+        Supports both random generation and from a fixed genparams dictionary.
+
+        Args:
+            image: Image to use as intensity prior if required.
+            segmentation: Segmentation to use as spatial prior.
+            seeds: Seeds to use for intensity generation.
+            genparams: Dictionary with generation parameters.
+                Used for fixed generation.
+                Should follow the structure and be of the same type as
+                the returned generation parameters.
+
+        Returns:
+            The synthetic augmented image, the segmentation, the original image, and the generation parameters.
+
+        """
+        # 1. Gamma contrast transformation
+        output, gamma_params = self.gamma(
+            output, self.device, genparams=genparams.get("gamma_params", {})
+        )
+
+        # 2. Bias field corruption
+        output, bf_params = self.biasfield(
+            output, self.device, genparams=genparams.get("bf_params", {})
+        )
+
+        # 3. Downsample to simulate lower reconstruction resolution
+        output, factors, resample_params = self.resampled(
+            output,
+            np.array(self.resolution),
+            self.device,
+            genparams=genparams.get("resample_params", {}),
+        )
+
+        # 4. Noise corruption
+        output, noise_params = self.noise(
+            output, self.device, genparams=genparams.get("noise_params", {})
+        )
+
+        # 5. Up-sample back to the original resolution/shape
+        output = self.resampled.resize_back(output, factors)
+
+        # 6. Induce SR-artifacts
+        artifacts = {}
+        for name, artifact in self.artifacts.items():
+            if artifact is not None:
+                output, metadata = artifact(
+                    output,
+                    segmentation,
+                    self.device,
+                    genparams.get("artifact_params", {}),
+                    resolution=self.resolution,
+                )
+                artifacts[name] = metadata
+
+        synth_params = {
+            "gamma_params": gamma_params,
+            "bf_params": bf_params,
+            "resample_params": resample_params,
+            "noise_params": noise_params,
+            "artifacts": artifacts,
+        }
+        return output, segmentation, image, synth_params
+
     def sample(
         self,
         image: torch.Tensor | None,
@@ -120,89 +261,23 @@ class FetalSynthGen:
         if genparams:
             genparams = self._validated_genparams(genparams)
 
-        # 1. Generate intensity output.
-        if seeds is not None:
-            seeds, selected_seeds = self.intensity_generator.load_seeds(
-                seeds=seeds, genparams=genparams.get("selected_seeds", {})
-            )
-            output, seed_intensities = self.intensity_generator.sample_intensities(
-                seeds=seeds,
-                device=self.device,
-                genparams=genparams.get("seed_intensities", {}),
-            )
-        else:
-            if image is None:
-                raise ValueError(
-                    "If no seeds are passed, an image must be loaded to be used as intensity prior!"
-                )
-            # normalize the image from 0 to 255 to
-            # match the intensity generator
-            output = (image - image.min()) / (image.max() - image.min()) * 255
-            selected_seeds = {}
-            seed_intensities = {}
-
-        # ensure that tensors are on the same device
-        output = output.to(self.device)
-        segmentation = segmentation.to(self.device)
-        image = image.to(self.device) if image is not None else None
-
-        # 2. Spatially deform the data
-        image, segmentation, output, deform_params = self.spatial_deform.deform(
+        # 1. Generate the deformed image
+        output, segmentation, image, synth_params = self.generate(
             image=image,
             segmentation=segmentation,
-            output=output,
-            genparams=genparams.get("deform_params", {}),
+            seeds=seeds,
+            genparams=genparams,
         )
 
-        # 3. Gamma contrast transformation
-        output, gamma_params = self.gamma(
-            output, self.device, genparams=genparams.get("gamma_params", {})
+        # 2. Augment the deformed image
+        output, segmentation, image, synth_params_aug = self.augment(
+            image=image,
+            segmentation=segmentation,
+            seeds=seeds,
+            genparams=genparams,
         )
 
-        # 4. Bias field corruption
-        output, bf_params = self.biasfield(
-            output, self.device, genparams=genparams.get("bf_params", {})
-        )
-
-        # 5. Downsample to simulate lower reconstruction resolution
-        output, factors, resample_params = self.resampled(
-            output,
-            np.array(self.resolution),
-            self.device,
-            genparams=genparams.get("resample_params", {}),
-        )
-
-        # 6. Noise corruption
-        output, noise_params = self.noise(
-            output, self.device, genparams=genparams.get("noise_params", {})
-        )
-
-        # 7. Up-sample back to the original resolution/shape
-        output = self.resampled.resize_back(output, factors)
-
-        # 8. Induce SR-artifacts
-        artifacts = {}
-        for name, artifact in self.artifacts.items():
-            if artifact is not None:
-                output, metadata = artifact(
-                    output,
-                    segmentation,
-                    self.device,
-                    genparams.get("artifact_params", {}),
-                    resolution=self.resolution,
-                )
-                artifacts[name] = metadata
-
-        # 9. Aggregete the synth params
-        synth_params = {
-            "selected_seeds": selected_seeds,
-            "seed_intensities": seed_intensities,
-            "deform_params": deform_params,
-            "gamma_params": gamma_params,
-            "bf_params": bf_params,
-            "resample_params": resample_params,
-            "noise_params": noise_params,
-            "artifacts": artifacts,
-        }
+        # 3. Aggregete the synth params
+        synth_params.update(synth_params_aug)
 
         return output, segmentation, image, synth_params
